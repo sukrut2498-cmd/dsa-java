5ff76208bc987151c18e640fc8fff28a2bf31c95
Sukrut Bilaskar <sukrut515@gmail.com>
2026-07-06 12:44:54 +0530

    Add LeetCode 49

---
+++ src/Hashing/GroupAnagramsLeetCode49.java
@@ -0,0 +1,39 @@
+package Hashing;
+
+import java.util.*;
+
+public class GroupAnagramsLeetCode49 {
+    public static void main(String[] args) {
+
+        String  strs[] = {"eat","tea","tan","ate","nat","bat"};
+        List<List<String>> lstr = groupAnagrams(strs);
+        System.out.println(lstr);
+
+    }
+    public static List<List<String>> groupAnagrams(String[] strs){
+        if(strs.length==0) {
+            return new ArrayList<>();
+        }
+        Map<String,List<String>> ansMap = new HashMap<>();
+        int [] count = new int[26];
+
+        for(String s:strs){
+            Arrays.fill(count,0);
+            for (char c:s.toCharArray()){
+                count[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder(" ");
+            for (int i=0; i<26; i++){
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if(!ansMap.containsKey(key)){
+                ansMap.put(key,new ArrayList<>());
+            }
+            ansMap.get(key).add(s);
+        }
+        return new ArrayList<>(ansMap.values());
+    }
+}
+
