62c55c81dd799798e542e44d8acbbb2ceeb535f0
Sukrut Bilaskar <sukrut515@gmail.com>
2026-07-09 23:53:57 +0530

    Add LeetCode 15

---
+++ src/TwoPointers/ThreeSumLeetCode15.java
@@ -0,0 +1,45 @@
+package TwoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSumLeetCode15 {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> ans = threeSum(nums);
+        System.out.println(ans);
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                twoSum2(nums, i, result);
+            }
+        }
+        return result;
+    }
+
+    public static void twoSum2(int[] nums, int i, List<List<Integer>> result) {
+        int left = i + 1;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                result.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                while (left < right && nums[left] == nums[left - 1]) {
+                    ++left;
+                }
+            }
+        }
+
+    }
+}
