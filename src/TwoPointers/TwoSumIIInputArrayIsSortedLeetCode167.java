f77ebc8ae3565b80d98cfcbf853acbbf4212574a
Sukrut Bilaskar <bsukrut407@gmail.com>
2026-07-09 18:51:05 +0530

    Update TwoSumIIInputArrayIsSortedLeetCode167.java

--- src/TwoPointers/TwoSumIIInputArrayIsSortedLeetCode167.java
+++ src/TwoPointers/TwoSumIIInputArrayIsSortedLeetCode167.java
@@ -13,7 +13,7 @@ public class TwoSumIIInputArrayIsSortedLeetCode167 {
         }
     }
 
-
+//Optimal Approach
     public static int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length-1;
